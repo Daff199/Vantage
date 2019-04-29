2b32fbe4a7143fbf29382e2232bf085e016bf1d0
daff199 <daaffexx@gmail.com>
2019-04-29 20:21:08 +0200

    Layout done
       - Sidebar added
       - Appbar added
       - Dynamic sidebar done

---
+++ js/ui.js
@@ -0,0 +1,38 @@
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+// console.log(triggerBtn);
+
+
+function toggleSidebar(){
+    if ($("#sidebar").classList.contains("active-sidebar")){
+        hideSidebar();
+        $("header").style.width = "100vw";
+    } else {
+        showSidebar();
+    }
+}
+
+function showSidebar() {
+    $("#sidebar").classList.toggle("hide-sidebar");
+    $("#sidebar").classList.add("active-sidebar");
+
+    $("#sidebar").style.width = "278px";
+    $("header").style.marginLeft = "278px";
+    $("#content").style.marginLeft = "278px";
+    
+    $("header").style.width = "85vw";
+};
+
+function hideSidebar() {
+    $("#sidebar").classList.toggle("active-sidebar");
+    $("#sidebar").classList.add("hide-sidebar");
+    $("#sidebar").style.width = "0";
+    $("#sidebar").style.marginLeft = "0";
+}
+window.onload = function () {
+    $("a.trigger").addEventListener("click", () => {
+        toggleSidebar();
+    })
+}
